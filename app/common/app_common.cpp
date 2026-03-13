1ec834efbd99d23ea1907f517770699b6dff6ddb
Your Name <you@example.com>
2026-03-13 21:05:16 +0300

    STM32 builded

--- app/common/app_common.cpp
+++ app/common/app_common.cpp
@@ -2,7 +2,16 @@
 #include "RotaryEncoder.h"
 #include "stdint.h"
 
-extern RotaryEncoder::listener_t enc_brg;
 gui_state_t gui_state = {0.0f, };
-RotaryEncoder encoder(enc_brg);
 volatile uint32_t systime = 0;
+//extern RotaryEncoder::listener_t enc_brg;
+//RotaryEncoder encoder(enc_brg);
+//
+//void dummy_hold() {debug_print("dummy hold\r");};
+//void dummy_timeout() {debug_print("dummy timeout\r");};
+//
+//void brg_rotate(int8_t delta);
+//void brg_click();
+//void brg_hold();
+//
+//RotaryEncoder::listener_t enc_brg = {brg_rotate, brg_click, brg_hold, dummy_timeout};
